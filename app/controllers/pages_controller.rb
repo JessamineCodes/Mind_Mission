cb3639dd29f383656a904b19d3e4a38dca7a0681
Jessamine McHugh <jessamine.mchugh@gmail.com>
2023-03-06 16:52:03 +0000

    finished level routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,40 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, except: [ :home, ]
 
   def home
+  end
+
+  def level_one_challenge
+
+  end
+
+
+  def level_one_snippet
+
+  end
+
+
+  def level_two_challenge
+
+  end
+
+
+  def level_two_snippet
+
+  end
+
+
+  def level_three_challenge
+
+  end
+
+
+  def level_three_snippet
+
+  end
+
+
+  def graduation
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  get 'level_one/challenge', to: 'pages#level_one_challenge'
+  get 'level_one/snippet', to: 'pages#level_one_snippet'
+
+
+  get 'level_two/challenge', to: 'pages#level_two_challenge'
+  get 'level_two/snippet', to: 'pages#level_two_snippet'
+
+  get 'level_three/challenge', to: 'pages#level_three_challenge'
+  get 'level_three/snippet', to: 'pages#level_three_snippet'
+
+  get 'graduation', to: 'pages#graduation'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

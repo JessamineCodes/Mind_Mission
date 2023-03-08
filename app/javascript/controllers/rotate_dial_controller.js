40b4b634496354c0b45766b3fdf5191a1ff2cad7
Jessamine McHugh <jessamine.mchugh@gmail.com>
2023-03-08 13:12:04 +0000

    disables button after clicking

--- app/javascript/controllers/rotate_dial_controller.js
+++ app/javascript/controllers/rotate_dial_controller.js
@@ -2,15 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="rotate-dial"
 export default class extends Controller {
-  static targets = ["dial"]
+  static targets = ["dial", "button"]
 
   connect() {
     console.log(this.dialTarget)
+    console.log(this.buttonTarget)
   }
 
   rotate() {
     console.log(this.dialTarget)
+
     this.dialTarget.classList.add("rotation-animation")
+    this.buttonTarget.classList.remove("button")
+    this.buttonTarget.classList.add("disabled-button")
+
     // this.dialTarget.style.transform = "rotate(144deg)";
     // this.dialTarget.style.transform = "rotate(144deg)";
 

62e13410c1868681d586e8cbe33a73502e43754b
Issy McCallion <issy.mccallion@gmail.com>
2023-03-08 15:23:23 +0000

    form working with js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import DisableButtonController from "./disable_button_controller"
+application.register("disable-button", DisableButtonController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 

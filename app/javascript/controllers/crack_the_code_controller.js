f2dcecdd1564eb608f77827658d6ee45bf6e6e0d
zac nation <zac.nation@icloud.com>
2023-03-08 16:17:55 +0000

    Basics of game work as shown in tutorial

--- app/javascript/controllers/crack_the_code_controller.js
+++ app/javascript/controllers/crack_the_code_controller.js
@@ -3,9 +3,49 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="crack-the-code"
 export default class extends Controller {
   connect() {
-    console.log("Connected to crack_the_code")
-  }
-  changeColor() {
-    console.log("Connected to change color action")
+    const words = [
+      {
+        word: "APPLE",
+        hint: "hint1"
+      },
+      {
+        word: "BANANA",
+        hint: "hint2"
+      }
+    ];
+
+    const wordText = document.querySelector(".word"),
+    hintText = document.querySelector(".hint span"),
+    inputField = document.querySelector("#user-input"),
+    solutionBtn = document.querySelector(".solution"),
+    checkBtn = document.querySelector(".check");
+
+    let correctWord;
+
+    const initGame = () => {
+      // get random object from words
+      let randomObj = words[Math.floor(Math.random() * words.length)];
+      let wordArray = randomObj.word.split("");
+      for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+      }
+      wordText.innerText = wordArray.join("");
+      hintText.innerText = randomObj.hint;
+      correctWord = randomObj.word.toLowerCase();
+      inputField.value = "";
+      inputField.setAttribute("maxlength", correctWord.length);
+    }
+    initGame();
+
+    const checkWord = () => {
+      let userWord = inputField.value.toLocaleLowerCase();
+      if (!userWord) return alert("Please enter a word");
+      if (userWord !== correctWord) return alert(`Oops! ${userWord} is not the correct word!`);
+      alert(`Yay! ${userWord.toUpperCase()} is the correct word!`)
+    }
+
+    solutionBtn.addEventListener("click", initGame);
+    checkBtn.addEventListener("click", checkWord);
   }
 }

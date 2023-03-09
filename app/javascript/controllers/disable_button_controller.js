bdc43117ccc62d12d70da98dca16ecb080538ea4
Jessamine McHugh <jessamine.mchugh@gmail.com>
2023-03-09 16:54:13 +0000

    updated level functions, added reward box to level 2, added first name to home page, added level to navbar

--- app/javascript/controllers/disable_button_controller.js
+++ app/javascript/controllers/disable_button_controller.js
@@ -2,13 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="disable-button"
 export default class extends Controller {
-  static targets = ["button", "input"]
+  static targets = ["button", "input", "reward"]
 
+  connect() {
+    console.log("Controller connected")
+  }
 
   enableButton(event) {
     if(this.#checkInputs().length === 0)  {
       this.buttonTarget.removeAttribute("disabled")
     }
+  }
+
+  openReward(event) {
+    event.preventDefault();
+    console.log("Open reward function working");
+    this.rewardTarget.style.display = "flex"
   }
 
   #checkInputs() {

5683f97edebf78cc5ce18cd80f9e4d144ee2a368
Jessamine McHugh <122129438+JessamineCodes@users.noreply.github.com>
2023-03-12 16:32:31 +0000

    added https redirect to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # Force a redirection from http:// to https:// URLs
+  config.force_ssl = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.

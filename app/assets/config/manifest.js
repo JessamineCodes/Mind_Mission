b55a6299360c420571d5ed873d080c2d78f3436a
zac nation <zac.nation@icloud.com>
2023-03-16 15:26:43 +0000

    Added sound to rest of game

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,9 @@
-//= link_tree ../images
+// = link_tree ../images
 // = link_directory ../stylesheets .css
-//= link_tree ../builds
-//= link sound.mp3
+// = link_tree ../builds
+// = link CompletedSound.mp3
+// = link EmptySound.mp3
+// = link SafeDialSound.mp3
+// = link WinSound.mp3
+// = link WrongSound.mp3
+// = link NextSound.mp3
